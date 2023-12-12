8809bb042cfe2c13d94dbf8a2bcf874f2c607e2f
l-o <helio.castrosp@outlook.com>
2023-12-12 00:01:42 -0300

    wrote something on the message input

--- pages/advertise_page.py
+++ pages/advertise_page.py
@@ -33,7 +33,7 @@ class AdvertisePage(BasePage):
             self.finder(self.input_phone).send_keys("11970459624")
             self.finder(self.input_email).send_keys("helio.castrosp@outlook.com")
             self.finder(self.input_website).send_keys("https://linkedin.com/castrohelio")
-            self.finder(self.input_message).send_keys("Hey there! hire me haha lol")
+            self.finder(self.input_message).send_keys("Hey there! hire me lol haha")
             time.sleep(3)
 
             
